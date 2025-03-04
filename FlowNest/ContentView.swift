60fee010216e4a5c287936d75920ea3d8bbdc0bc
Vishnu Hari <vishnu.hari@trenser.com>
2025-03-04 22:23:35 +0530

    Initial Commit

---
+++ FlowNest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlowNest
+//
+//  Created by Trenser01 on 3/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlowNest/FlowNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowNestApp.swift
+//  FlowNest
+//
+//  Created by Trenser01 on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FlowNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
